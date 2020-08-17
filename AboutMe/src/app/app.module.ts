c6f7545835611c93b7c4e37fe46563ee43f29cd1
Tou Yang <yato0501@gmail.com>
2020-08-16 23:01:58 -0500

    finally got angular material installed correctly

--- AboutMe/src/app/app.module.ts
+++ AboutMe/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatTabsModule} from '@angular/material/tabs';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
