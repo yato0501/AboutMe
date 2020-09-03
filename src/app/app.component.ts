259c3a76ab481dadca497e5f8b4507e13aa8aef3
Tou Yang <yato0501@gmail.com>
2020-09-02 23:14:08 -0500

    added social media logos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AboutMe';
+
+  constructor() {
+
+  }
+
+  public GotoLinkedIn() : void {
+    window.open('https://www.linkedin.com/in/tou-yang-8a88a059/', '_blank');
+  }
+
+  
+  public GotoGitHub() : void {
+    window.open('https://github.com/yato0501', '_blank');
+  }
 }
